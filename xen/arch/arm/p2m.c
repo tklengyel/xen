cbce2a58a49ac10a48630b31f18c61b246993bf0
Sergej Proskurin <proskurin@sec.in.tum.de>
2016-08-01 08:36:30 +0200

    arm/p2m: Cosmetic fix - function prototype of p2m_alloc_table
    The function "p2m_alloc_table" should be able to allocate 2nd stage
    translation tables not only for the host's p2m but also for alternate
    p2m's.
    
    Signed-off-by: Sergej Proskurin <proskurin@sec.in.tum.de>
    Acked-by: Julien Grall <julien.grall@arm.com>
    ---
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    Cc: Julien Grall <julien.grall@arm.com>
    ---
    v2: Removed altp2m table initialization from "p2m_table_init".
    
    v3: Removed initialization of the field d->arch.altp2m_active in
        "p2m_table_init" to avoid altp2m initialization throughout different
        files.
    
        Merged the function "p2m_alloc_table" and "p2m_table_init".

--- xen/arch/arm/p2m.c
+++ xen/arch/arm/p2m.c
@@ -1138,9 +1138,8 @@ void guest_physmap_remove_page(struct domain *d,
     p2m_remove_mapping(d, gfn, (1 << page_order), mfn);
 }
 
-static int p2m_alloc_table(struct domain *d)
+static int p2m_alloc_table(struct p2m_domain *p2m)
 {
-    struct p2m_domain *p2m = p2m_get_hostp2m(d);
     struct page_info *page;
     unsigned int i;
 
@@ -1293,7 +1292,7 @@ int p2m_init_one(struct domain *d, struct p2m_domain *p2m)
     p2m->clean_pte = iommu_enabled &&
         !iommu_has_feature(d, IOMMU_FEAT_COHERENT_WALK);
 
-    return p2m_alloc_table(d);
+    return p2m_alloc_table(p2m);
 }
 
 static void p2m_teardown_hostp2m(struct domain *d)
