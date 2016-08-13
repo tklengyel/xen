8a20897da6155df14203780d1cbf52c427a9265f
Sergej Proskurin <proskurin@sec.in.tum.de>
2016-08-13 12:51:45 +0200

    arm/p2m: Introduce p2m_lookup_attr
    The function "p2m_lookup_attr" allows to lookup the mfn, memory type,
    access rights, and page order corresponding to a domain's gfn.
    
    Signed-off-by: Sergej Proskurin <proskurin@sec.in.tum.de>
    ---
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    Cc: Julien Grall <julien.grall@arm.com>
    ---
    v3: Change function prototype of "p2m_lookup_attr" by removing the
        function parameter "unsigned int *mattr", as it is not needed by the
        callers.
    
        Change function prototype of "p2m_lookup_attr" by changing the
        parameter of type xenmem_access_t to p2m_access_t.

--- xen/arch/arm/p2m.c
+++ xen/arch/arm/p2m.c
@@ -429,6 +429,21 @@ mfn_t p2m_lookup(struct domain *d, gfn_t gfn, p2m_type_t *t)
     return ret;
 }
 
+mfn_t p2m_lookup_attr(struct p2m_domain *p2m,
+                      gfn_t gfn,
+                      p2m_type_t *t,
+                      p2m_access_t *a,
+                      unsigned int *page_order)
+{
+    mfn_t ret;
+
+    p2m_read_lock(p2m);
+    ret = p2m_get_entry(p2m, gfn, t, a, page_order);
+    p2m_read_unlock(p2m);
+
+    return ret;
+}
+
 int guest_physmap_mark_populate_on_demand(struct domain *d,
                                           unsigned long gfn,
                                           unsigned int order)
--- xen/include/asm-arm/p2m.h
+++ xen/include/asm-arm/p2m.h
@@ -196,6 +196,16 @@ void p2m_dump_info(struct domain *d);
 /* Look up the MFN corresponding to a domain's GFN. */
 mfn_t p2m_lookup(struct domain *d, gfn_t gfn, p2m_type_t *t);
 
+/*
+ * Lookup the MFN, memory type, access rights, and page table level
+ * corresponding to a domain's GFN.
+ */
+mfn_t p2m_lookup_attr(struct p2m_domain *p2m,
+                      gfn_t gfn,
+                      p2m_type_t *t,
+                      p2m_access_t *a,
+                      unsigned int *page_order);
+
 /* Remove an altp2m view's entry. */
 int remove_altp2m_entry(struct p2m_domain *p2m,
                         gfn_t gfn,
