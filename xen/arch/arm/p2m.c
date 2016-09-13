52e94b182874ffddc7241c6c0098e320227b8d4a
Sergej Proskurin <proskurin@sec.in.tum.de>
2016-09-13 10:54:32 +0200

    arm/p2m: Cosmetic fix - substitute _gfn(ULONG_MAX) for INVALID_GFN
    In ./xen/arch/arm/p2m.c, we compare the gfn's with INVALID_GFN
    throughout the code. Thus it makes sense to use the macro INVALID_GFN
    instead of a hard coded value to initialize "p2m->lowest_mapped_gfn".
    
    Signed-off-by: Sergej Proskurin <proskurin@sec.in.tum.de>
    ---
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    Cc: Julien Grall <julien.grall@arm.com>

--- xen/arch/arm/p2m.c
+++ xen/arch/arm/p2m.c
@@ -1253,7 +1253,7 @@ int p2m_init(struct domain *d)
         return rc;
 
     p2m->max_mapped_gfn = _gfn(0);
-    p2m->lowest_mapped_gfn = _gfn(ULONG_MAX);
+    p2m->lowest_mapped_gfn = INVALID_GFN;
 
     p2m->default_access = p2m_access_rwx;
     p2m->mem_access_enabled = false;
