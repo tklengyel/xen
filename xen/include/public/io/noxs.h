05071a6706b53df62606e0e350023625043be43b
Filipe Manco <filipe.manco@neclab.eu>
2017-05-26 11:30:37 +0200

    noxs: add vif device

--- xen/include/public/io/noxs.h
+++ xen/include/public/io/noxs.h
@@ -23,6 +23,7 @@ typedef uint32_t noxs_dev_id_t;
 enum noxs_dev_type {
 	noxs_dev_none = 0,
 	noxs_dev_sysctl,
+	noxs_dev_vif,
 };
 typedef enum noxs_dev_type noxs_dev_type_t;
 
@@ -103,5 +104,72 @@ struct noxs_sysctl_ctrl_page {
 	};
 };
 typedef struct noxs_sysctl_ctrl_page noxs_sysctl_ctrl_page_t;
+
+/* vif device */
+struct vif_be_features {
+	uint8_t rx_notify:1;
+	uint8_t sg:1;
+	uint8_t gso_tcpv4:1;
+	uint8_t gso_tcpv4_prefix:1;
+	uint8_t gso_tcpv6:1;
+	uint8_t gso_tcpv6_prefix:1;
+	uint8_t no_csum_offload:1;
+	uint8_t ipv6_csum_offload:1;
+	uint8_t rx_copy:1;
+	uint8_t rx_flip:1;
+	uint8_t multicast_control:1;
+	uint8_t dynamic_multicast_control:1;
+	uint8_t split_event_channels:1;
+	uint8_t ctrl_ring:1;
+	uint8_t netmap:1;
+};
+
+struct vif_fe_features {
+	uint8_t rx_notify:1;
+	uint8_t persistent:1;
+	uint8_t sg:1;
+	uint8_t gso_tcpv4:1;
+	uint8_t gso_tcpv4_prefix:1;
+	uint8_t gso_tcpv6:1;
+	uint8_t gso_tcpv6_prefix:1;
+	uint8_t no_csum_offload:1;
+	uint8_t ipv6_csum_offload:1;
+	uint8_t ctrl_ring:1;
+};
+
+#define ETH_LEN    6       /* Octets in one ethernet address */
+#define IF_LEN     16
+
+struct noxs_vif_ctrl_page {
+	noxs_ctrl_hdr_t hdr;
+	int vifid;
+	struct vif_be_features be_feat;
+	int multi_queue_max_queues;
+	int multi_queue_num_queues;
+
+	grant_ref_t tx_ring_ref;
+	grant_ref_t rx_ring_ref;
+	evtchn_port_t event_channel_tx;
+	evtchn_port_t event_channel_rx;
+
+	unsigned int request_rx_copy;
+	struct vif_fe_features fe_feat;
+
+	grant_ref_t ctrl_ring_ref;
+	evtchn_port_t event_channel_ctrl;
+
+	uint8_t mac[ETH_LEN];
+	uint32_t ip;
+	char bridge[IF_LEN];
+};
+typedef struct noxs_vif_ctrl_page noxs_vif_ctrl_page_t;
+
+
+struct noxs_cfg_vif {
+	uint8_t mac[ETH_LEN];
+	uint32_t ip;
+	char bridge[IF_LEN];
+};
+typedef struct noxs_cfg_vif noxs_cfg_vif_t;
 
 #endif /* XEN_PUBLIC_IO_NOXS_H_ */
